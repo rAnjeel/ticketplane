3933dac1dce72825ace24c29e465a28e418c1e2e
Angelo RABEARISON <151778901+rAnjeel@users.noreply.github.com>
2025-04-01 22:29:43 +0300

    Creation du corps du projet
    Initialisation des modules ainsi que le corps du projet d'extension

---
+++ extented/src/main/java/com/ticketing/extented/ExtentedApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.extented;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtentedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtentedApplication.class, args);
+	}
+
+}
